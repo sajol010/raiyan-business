12c1021ffdc4d59887f1f26ab05ca367d6439a4f
Sajol Mahmud <sajolmahmud010@gmail.com>
2023-03-01 11:38:04 +0600

    approve sms sent

--- app/Http/Controllers/Backend/ApplicationController.php
+++ app/Http/Controllers/Backend/ApplicationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Certificate;
+use App\Services\SmsService;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
@@ -11,8 +12,23 @@ class ApplicationController extends Controller
     public function approve($id){
         $application = Certificate::find($id);
         $application->status = 1;
+        $this->sendSms($application);
         $application->save();
+
+
+
+
         return back();
+    }
+
+    public function sendSms($application){
+        $mobileNumber = !empty($application['b_phone'])?$application['b_phone']:$application['e_phone'];
+        if (empty($application)){
+            return;
+        }
+        $sms = new SmsService();
+        $sms->setTemplate("আপনার আবেদন অনুমোদিত হয়েছে");
+        $sms->sendSms($mobileNumber);
     }
 
     public function reject($id){
